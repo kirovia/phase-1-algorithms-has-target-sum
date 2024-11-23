7f9165f27436a45aaac8ceee5092c2e8ed09a413
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-23 02:17:46 -0500

    passes all tests

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+/* 
+  
+PROBLEM
+I need to write a function that will take two arguments. The first argument will be an array of numbers and the second argument will be a single number. This function is going to check to see if any two numbers in the array can be summed to equal the second argument, returning true if so and false if not
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PSEUDOCODE
+function iterates over the array of numbers
+for each item in array, iterate over numbers again
+add the index of the first iteration with the index of the second iteration
+return does index1 + index2 === second argument?
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i1 = 0; i1 < array.length; i1++) {
+    for (let i2 = 0; i2 < array.length; i2++) {
+      if (array[i1] + array[i2] === target && array.indexOf(array[i1], i1) !== array.indexOf(array[i2], i2)) {
+        return true;
+      }
+    }
+  }
+  return false
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
 /*
-  Add written explanation of your solution here
+
+CODE EXPLANATION
+The code is maybe not optimal, in fact I think it is O(n²), but it runs as expected. The function runs two for loops, first checking to see if two numbers match each other, but we cannot match a number with itself to sum the target number. That is why I incorporated that the numbers have to sum the target AND not be located at the same index (cannot be the same number duplicated and added to itself)
+
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
